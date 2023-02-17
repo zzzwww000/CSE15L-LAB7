a77c08ab428f3089d8f6af278ad4f4bf6c63f980
enzohnnn <47432341+enzohnnn@users.noreply.github.com>
2023-02-17 14:31:55 -0800

    Update ListExamplesTests.java

--- ListExamplesTests.java
+++ ListExamplesTests.java
@@ -5,14 +5,14 @@ import java.util.ArrayList;
 
 
 public class ListExamplesTests {
-	@Test 
+	@Test(timeout = 500)
 	public void testMerge1() {
     		List<String> l1 = new ArrayList<String>(Arrays.asList("x", "y"));
 		List<String> l2 = new ArrayList<String>(Arrays.asList("a", "b"));
 		assertArrayEquals(new String[]{ "a", "b", "x", "y"}, ListExamples.merge(l1, l2).toArray());
 	}
 	
-	@Test
+	@Test(timeout = 500)
         public void testMerge2() {
 		List<String> l1 = new ArrayList<String>(Arrays.asList("a", "b", "c"));
 		List<String> l2 = new ArrayList<String>(Arrays.asList("c", "d", "e"));
