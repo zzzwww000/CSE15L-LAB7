101f5b798158c2bb4244d111fd9dba7e374dc32d
enzohnnn <47432341+enzohnnn@users.noreply.github.com>
2023-02-17 14:20:40 -0800

    Update ListExamplesTests.java

--- ListExamplesTests.java
+++ ListExamplesTests.java
@@ -1,21 +1,21 @@
 import static org.junit.Assert.*;
 import org.junit.*;
-import java.util.List;
+import java.util.*;
 import java.util.ArrayList;
 
 
 public class ListExamplesTests {
 	@Test 
 	public void testMerge1() {
-    		List<String> l1 = new ArrayList<String>(List.of("x", "y"));
-		List<String> l2 = new ArrayList<String>(List.of("a", "b"));
+    		List<String> l1 = new ArrayList<String>(Arrays.asList("x", "y"));
+		List<String> l2 = new ArrayList<String>(Arrays.asList("a", "b"));
 		assertArrayEquals(new String[]{ "a", "b", "x", "y"}, ListExamples.merge(l1, l2).toArray());
 	}
 	
 	@Test
         public void testMerge2() {
-		List<String> l1 = new ArrayList<String>(List.of("a", "b", "c"));
-		List<String> l2 = new ArrayList<String>(List.of("c", "d", "e"));
+		List<String> l1 = new ArrayList<String>(Arrays.asList("a", "b", "c"));
+		List<String> l2 = new ArrayList<String>(Arrays.asList("c", "d", "e"));
 		assertArrayEquals(new String[]{ "a", "b", "c", "c", "d", "e" }, ListExamples.merge(l1, l2).toArray());
         }
 
